dfed564502a2ed4033017df30bf010a325d92363
narendrajava1 <narendrakolli08@gmail.com>
2023-07-01 20:19:51 +0530

    initial commit

---
+++ src/test/java/com/naren/SpringbootMapstructEmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMapstructEmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
