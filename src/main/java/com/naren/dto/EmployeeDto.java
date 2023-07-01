5851da1822dd947fc30e82cd476eda7148e701f1
narendrajava1 <narendrakolli08@gmail.com>
2023-07-02 01:38:43 +0530

    added swagger annotations on DTO classes

--- src/main/java/com/naren/dto/EmployeeDto.java
+++ src/main/java/com/naren/dto/EmployeeDto.java
@@ -1,18 +1,35 @@
 package com.naren.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "EmployeeDto Model Information"
+)
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeDto {
     private Long id;
+    @Schema(
+            description = "Employee First Name"
+    )
     private String firstName;
+    @Schema(
+            description = "Employee Last Name"
+    )
     private String lastName;
+    
+    @Schema(
+            description = "Employee Email Address"
+    )
     private String email;
+    @Schema(
+            description = "Employee Department Address"
+    )
     private String departmentCode;
 }
